80ca2723a2c2b3ce875ef4b35773c12a8ba16264
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-04-30 15:23:54 -0600

    renamed file

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,7 +1,11 @@
 const Employee = require('../lib/Employee');
 
-describe('Employee', () => {
 
-
-  
-})
+describe("Employee", () => {
+  describe("name", () => {
+    it("should return a 'string' not a number.", () => {
+      const obj = new Employee();
+      expect("string" in obj).toEqual(true);
+    }); 
+  });
+});
